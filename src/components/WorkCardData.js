0e144f53993efacb2606bbb3d69902862c041508
jyoti <jyotikumari123>
2023-05-30 20:30:06 +0530

    initial commit

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,6 +3,7 @@ import pro1 from "../assets/project1.jpg";
 import pro2 from "../assets/project2.jpg";
 
 import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project4.jpg";
 
 const ProjectCardData = [
     {
@@ -15,15 +16,22 @@ const ProjectCardData = [
      {
         imgsrc : pro2,
         title : "Meal Planner",
-        text : "hdfuygsdbnss",
+        text : "A web application which takes user data and generates meal plan for them along with their recipes.",
         view : "https://meal-planner-7200ab.netlify.app/"
     },
 
      {
         imgsrc : pro3,
         title : "Flappy Bug",
-        text : "hdfuygsdbn",
+        text : "This is a complete responsive game with click handlers to make the bug jump across the random generated pipes, detect collision between the bug and the pipe, and add a running score.",
         view : "https://flappy-bug.netlify.app/"
+    },
+
+    {
+        imgsrc : pro4,
+        title : "Flappy Bug",
+        text : "This ia a web application which allow users to search for and browse latest and upcoming movies.",
+        view : "https://imdb-clone-22f717.netlify.app"
     }
 ];
 
